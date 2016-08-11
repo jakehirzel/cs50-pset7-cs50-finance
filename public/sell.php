b8ca91320ac02893e37205b605a7c67e2bb39caa
Jacob Hirzel <hirzel@hotmail.com>
2016-08-11 20:23:38 +0000

    Fished sell by adding page refresh and update of cash available.

--- public/sell.php
+++ public/sell.php
@@ -20,37 +20,38 @@
         // Delete the row from the portfolio
         CS50::query("DELETE FROM portfolios WHERE id = ?", $_POST["id"]);
         
+        // Update cash available
+        CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?", $sale_value, $_SESSION["id"]);
+        
     }
     
-    else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Query for portfolio and save to session
+    $portfolio = CS50::query("SELECT * FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
+    $_SESSION["portfolio"] = $portfolio;
+    
+    // Update share prices
+    foreach ($_SESSION["portfolio"] as &$stock) {
         
-        // Query for portfolio and save to session
-        $portfolio = CS50::query("SELECT * FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
-        $_SESSION["portfolio"] = $portfolio;
-        
-        // Update share prices
-        foreach ($_SESSION["portfolio"] as &$stock) {
-            
-            // Validate
-            $current_price = lookup($stock["symbol"]);
-            if ($current_price == 0) {
-                apologize("Invalid stock symbol. Unable to refresh portfolio.");
-            }
-            
-            // Assign return value of lookup to current_price
-            else {
-                $stock["current_price"] = $current_price["price"];
-            }
+        // Validate
+        $current_price = lookup($stock["symbol"]);
+        if ($current_price == 0) {
+            apologize("Invalid stock symbol. Unable to refresh portfolio.");
         }
         
-        // Unset the referenced value
-        unset($stock);
-        
-        // Query for cash available and save to session
-        $cash_available = CS50::query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
-        $_SESSION["cash_available"] = $cash_available[0]["cash"];
-        
-        // Render sell_form.php
-        render("sell_form.php", ["title" => "Sell"]);
+        // Assign return value of lookup to current_price
+        else {
+            $stock["current_price"] = $current_price["price"];
+        }
     }
+    
+    // Unset the referenced value
+    unset($stock);
+    
+    // Query for cash available and save to session
+    $cash_available = CS50::query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
+    $_SESSION["cash_available"] = $cash_available[0]["cash"];
+    
+    // Render sell_form.php
+    render("sell_form.php", ["title" => "Sell"]);
+    
 ?>
