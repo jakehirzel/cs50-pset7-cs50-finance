92697dbefad27247b4eee313532e9599f57cd837
Jacob Hirzel <hirzel@hotmail.com>
2016-08-11 15:09:10 +0000

    Fixed foreach in index.php and correctly getting current_price in portfolio.php.

--- public/index.php
+++ public/index.php
@@ -14,19 +14,23 @@
         $portfolio = CS50::query("SELECT * FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
         $_SESSION["portfolio"] = $portfolio;
         
-        // Update share prices in portfolio
-        foreach ($_SESSION["portfolio"] as $stock) {
+        // Update share prices
+        foreach ($_SESSION["portfolio"] as &$stock) {
             
             // Validate
-            if (lookup($stock["symbol"]) == 0) {
+            $current_price = lookup($stock["symbol"]);
+            if ($current_price == 0) {
                 apologize("Invalid stock symbol. Unable to refresh portfolio.");
             }
             
             // Assign return value of lookup to current_price
             else {
-                $stock["current_price"] = lookup($stock["symbol"] );
+                $stock["current_price"] = $current_price["price"];
             }
         }
+        
+        // Unset the referenced value
+        unset($stock);
         
         // Render portfolio
         render("portfolio.php", ["title" => "Portfolio"]);
--- views/portfolio.php
+++ views/portfolio.php
@@ -1,5 +1,7 @@
 <h1>Portfolio for <b><?= $_SESSION["username"] ?></b></h1>
 
 <div class="row">
-    <div class="col-xs-12"><b><?= $_SESSION["portfolio"][0]["symbol"] ?></b> <?= $_SESSION["portfolio"][0]["shares"] ?> @ <?= $_SESSION["portfolio"][0]["current_price"] ?> = <?= $_SESSION["portfolio"][0]["shares"] * $_SESSION["portfolio"][0]["current_price"] ?></div>
+    <div class="col-xs-12">
+        <b><?= $_SESSION["portfolio"][0]["symbol"] ?></b> <?= $_SESSION["portfolio"][0]["shares"] ?> @ <?= $_SESSION["portfolio"][0]["current_price"] ?> = <?= $_SESSION["portfolio"][0]["shares"] * $_SESSION["portfolio"][0]["current_price"] ?>
+    </div>
 </div>
