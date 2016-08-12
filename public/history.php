bdfa5b7f7a7bdbed14a0223f8476999353649cff
Jacob Hirzel <hirzel@hotmail.com>
2016-08-12 14:41:54 +0000

    Moved timezone calculations to history.php; left open for possible timezone customization later.

--- public/history.php
+++ public/history.php
@@ -14,6 +14,21 @@
         $history = CS50::query("SELECT * FROM history WHERE user_id = ?", $_SESSION["id"]);
         $_SESSION["history"] = $history;
         
+        // Get timezone offset (per http://bit.ly/2bcg4tt)
+        $new_timezone = new DateTimeZone('America/New_York');
+        $time_in_timezone = new DateTime('now', $new_timezone);
+        $offset = $new_timezone->getOffset($time_in_timezone);
+        
+        // Update timezone and format date
+        foreach ($_SESSION["history"] as &$item) {
+            $time_adjusted = date("D, M dS g:ia", strtotime($item["timestamp"] . $offset . " seconds"));
+            $item["timestamp"] = $time_adjusted;
+            
+        }
+        
+        // Unset referenced value
+        unset($item);
+        
         // Render history display
         render("history_display.php", ["title" => "History"]);
         
--- views/history_display.php
+++ views/history_display.php
@@ -4,7 +4,7 @@
     <div class="col-xs-12">
         <?php 
             foreach ($_SESSION["history"] as $item) {
-                print("<p><i>" . date("D, M dS g:ia", strtotime($item["timestamp"] . " - 14400 seconds")) . "</i> " . $item["type"] . " " . floor($item["shares"]) . " share(s) of <b>" . htmlspecialchars($item["symbol"]) . "</b> for $"  . number_format($item["amount"], $decimals = 2, $dec_point = ".", $thousands_sep = ","));
+                print("<p><i>" . $item["timestamp"] . "</i> " . $item["type"] . " " . floor($item["shares"]) . " share(s) of <b>" . htmlspecialchars($item["symbol"]) . "</b> for $"  . number_format($item["amount"], $decimals = 2, $dec_point = ".", $thousands_sep = ","));
             }
         ?>
     </div>
